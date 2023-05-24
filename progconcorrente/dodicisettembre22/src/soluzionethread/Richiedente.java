2079a4d5abc54ce7be903dc7ac85dfb2fe85c31b
luca.canali <canali.luca998@gmail.com>
2023-05-24 10:03:31 +0200

    Upload dodicisettembre22

---
+++ progconcorrente/dodicisettembre22/src/soluzionethread/Richiedente.java
@@ -0,0 +1,37 @@
+package soluzionethread;
+
+public class Richiedente extends Thread {
+	int mioId;
+	Deposito depositoIn;
+	Deposito depositoOut;
+	public Richiedente(int id, Deposito dIn, Deposito dOut) {
+		mioId=id;
+		depositoIn=dIn;
+		depositoOut=dOut;
+		start();
+	}
+	public void run() {
+		final int numIterazioni=3;
+		String richiesta;
+		RichiestaRisposta ricrisp;
+		for(int i=0; i<numIterazioni; i++) {
+			int richId=mioId*10000+i;
+			richiesta="richiesta"+richId;
+			ricrisp=new RichiestaRisposta(richId, richiesta);
+			System.out.println("Richiedente "+mioId+" inserisco "+richiesta);
+			depositoIn.Aggiungi(ricrisp);
+			boolean pronto=false;
+			while(!pronto) {
+				ricrisp=depositoOut.LeggiIdx(richId);
+				if(ricrisp!=null) {
+					pronto=true;
+				} else {
+					System.out.println("Richiedente "+mioId+" dormo ");
+					try { Thread.sleep(1000); } catch (InterruptedException e) { }
+				}
+			}
+			System.out.println("Richiedente "+mioId+" Q: "+ricrisp.getRichiesta()+", A: "+ricrisp.getRisposta());
+		}
+		System.out.println("Richiedente "+mioId+" termina ");
+	}
+}
---
+++ progconcorrente/dodicisettembre22/src/soluzionethread/TheMain.java
@@ -0,0 +1,22 @@
+package soluzionethread;
+
+// soluzione bis
+// bloccante la lettura(idx)
+public class TheMain {
+
+	final int numRichiedenti=3;
+	final int numEsecutori=3;
+	Deposito depositoIn = new Deposito();
+	Deposito depositoOut = new Deposito();
+	public void exec() {
+		for(int i=0; i<numEsecutori; i++) {
+			new Esecutore(i, depositoIn, depositoOut);
+		}
+		for(int i=0; i<numRichiedenti; i++) {
+			new Richiedente(i, depositoIn, depositoOut);
+		}	
+	}
+	public static void main(String[] args) {
+		new TheMain().exec();
+	}
+}
