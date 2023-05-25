9a2c102cc01e5d26e3c1aaf106594ef3f2b6157c
luca.canali <canali.luca998@gmail.com>
2023-05-25 15:00:34 +0200

    update

---
+++ progconcorrente/seigiugno22/src/soluzionesocket/Elemento.java
@@ -0,0 +1,16 @@
+package soluzionesocket;
+
+import java.io.Serializable;
+
+public class Elemento implements Serializable{
+	private static final long serialVersionUID = 1L;
+	String descrizione;
+	int quantita;
+	Elemento(String s, int q){
+		descrizione=s;
+		quantita=q;
+	}
+	public String toString() {
+		return "element "+descrizione+" ("+quantita+")";
+	}
+}
